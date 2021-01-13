15a6969a2bceb96ef7e9cab5b55e7bb463577f5b
Sebi Unipan <sunipan@gmail.com>
2021-01-12 17:21:04 -0800

    Updated jar file and .exe file to reflect the bug fix

--- src/AutofillTest.java
+++ src/AutofillTest.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import org.junit.Test;
+
 public class AutofillTest {
-	
+	@Test
 	public static void main(String[] args) throws FileNotFoundException, CryptoException {
 		//create Autofill object
 		Autofill obj = new Autofill();
@@ -16,21 +18,21 @@ public class AutofillTest {
 		System.out.println("2 - Find a pass.");
 		System.out.println("3 - Remove pass.");
 		System.out.println("0 - Exit");
-		int answer = in.nextInt();
-		while(answer != 0) {
-			if(answer == 1) {
+		String answer = in.next();
+		while(!answer.equals("0")) {
+			if(answer.equals("1")) {
 				System.out.println("What is the username?");
 				String username = in.next();
 				System.out.println("What is the password?");	//append new user info to file
 				String pass = in.next();
 				obj.addPass(username, pass);
 			}
-			else if(answer == 2) {
+			else if(answer.equals("2")) {
 				System.out.println("What's the username of the password you're looking for?"); 
 				String username = in.next();
 				obj.findPass(username);
 			}
-			else if(answer == 3) {
+			else if(answer.equals("3")) {
 				System.out.println("Enter the username for the password you'd like to remove.");
 				String username = in.next();
 				obj.deletePass(username);
@@ -42,7 +44,7 @@ public class AutofillTest {
 			System.out.println("2 - Find a pass.");
 			System.out.println("3 - Remove pass.");
 			System.out.println("0 - Exit");
-			answer = in.nextInt(); 
+			answer = in.next(); 
 		}
 	}
 }
